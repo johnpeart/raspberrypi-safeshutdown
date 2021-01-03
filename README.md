c405d3e15cee1b7145e91713c58261dde0b5ed14
John Peart <code@johnpe.art>
2021-01-03 21:43:45 +0000

    Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # raspberrypi-safeshutdown
+
 A script for initiating a safe shutdown on a Raspberry Pi when a button, attached to GPIO pins, is pressed.
+
+
+## Acknowledgments
+
+Based on tutorial from [Core Electronics](https://core-electronics.com.au/tutorials/how-to-make-a-safe-shutdown-button-for-raspberry-pi.html)
---
+++ safe-shutdown.py
@@ -0,0 +1,15 @@
+from gpiozero import Button #import button from the Pi GPIO library
+import time # import time functions
+import os #imports OS library for Shutdown control
+
+stopButton = Button(3) # defines the button as an object and chooses GPIO 3
+
+# This script will run every 10 seconds. If the button is pressed, 
+# it will shutdown the Raspberry Pi and put it into a low power state.
+# Because the script only runs every 10 seconds, you need to keep 
+# hold of the button for up to 10 seconds to initiate a shutdown.
+
+while True: #infinite loop
+    if stopButton.is_pressed: # Check to see if button is pressed
+        os.system("halt") # shut down the Pi
+    time.sleep(10) # wait to loop again so we don’t use the processor too much.
